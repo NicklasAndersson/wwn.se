7329c35acbaf6a3840ca36cfffa57f4aaf2f94ec
nicklas <nicklas.andersson@leanon.se>
2019-01-28 16:33:01 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+ssh oderland 'mkdir -p /home2/nicklasa/wwn.se/images'
+scp -r images/*.png oderland:/home2/nicklasa/wwn.se/images
+
+ssh oderland 'mkdir -p /home2/nicklasa/wwn.se/inter-ui-3-2'
+scp -pr inter-ui-3-2/*.{woff,woff2,css} oderland:/home2/nicklasa/wwn.se/inter-ui-3-2
+
+scp *.{html,scss,css} oderland:/home2/nicklasa/wwn.se
+
