f1b378556c904118f96d7777fa51972a75d7232d
nicklas <nicklas.andersson@leanon.se>
2019-02-13 10:49:26 +0100

    Cleanup

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,3 @@ ssh oderland 'rm /home2/nicklasa/wwn.se/*.png'
 ssh oderland 'rm /home2/nicklasa/wwn.se/*.js'
 ssh oderland 'rm /home2/nicklasa/wwn.se/*.html'
 scp dist/*.{html,png,js} oderland:/home2/nicklasa/wwn.se
-
-
